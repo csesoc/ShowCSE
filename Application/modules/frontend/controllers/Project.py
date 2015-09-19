cdff5b90568a1076c54a313dae459b681c56c7e2
Nicholas Whyte <nick@nickwhyte.com>
2015-09-19 21:58:08 +1000

    Meow

--- Application/modules/frontend/controllers/Project.py
+++ Application/modules/frontend/controllers/Project.py
@@ -1,5 +1,5 @@
 from flask.ext.classy import FlaskView, route
-from flask import render_template
+from flask import render_template, request
 from flask_menu.classy import classy_menu_item
 from flask_login import login_required
 from .forms import SubmitProjectForm
@@ -11,6 +11,10 @@ class Project(FlaskView):
 
     @classy_menu_item('frontend-right.submit', 'Submit', order=0)
     @login_required
+    @route('/submit/', methods=['GET','POST'])
     def submit(self):
         form = SubmitProjectForm()
+        print(request.files)
+        if form.validate_on_submit():
+            print("dksjhfdskjhf")
         return render_template('.project/submit.html', form=form)
