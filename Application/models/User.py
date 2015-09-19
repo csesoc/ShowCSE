786a3c70e513a17323ec84a12c76929fc95f0ae1
Nicholas Whyte <nick@nickwhyte.com>
2015-09-19 18:28:03 +1000

    jake, here have a TODO

--- Application/models/User.py
+++ Application/models/User.py
@@ -1,6 +1,7 @@
 from Application import db
+from flask_login import UserMixin
 
-class User(db.Document):
+class User(db.Document, UserMixin):
 	dn = db.StringField()
 	zid = db.StringField()
 	firstname = db.StringField()
@@ -11,4 +12,5 @@ class User(db.Document):
 	email = db.StringField()
 	program = db.StringField()
 	admin = db.BoolField()
-	projects = db.SetField(db.DocumentField())
+	# projects = db.SetField(db.DocumentField())
+	# TODO JB: TypeError: __init__() missing 1 required positional argument: 'document_class'
