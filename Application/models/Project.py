43f343065c7389e6efa78e3c430944c547d836ef
jake-waratahs <jakebloom744@gmail.com>
2015-09-19 18:13:50 +1000

    user and project models

--- Application/models/Project.py
+++ Application/models/Project.py
@@ -1,4 +1,8 @@
 from Application import db
 
 class Project(db.Document):
-	pass
+	name = db.StringField()
+	date_uploaded = db.CreatedField()
+	primary_image = db.AnythingField(required=False)
+	images = db.ListField()
+	devs = db.SetField(db.DocumentField())
