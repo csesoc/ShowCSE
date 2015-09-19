562a4bbe66527a84a5b6657ea0f236df69e192ac
Nicholas Whyte <nick@nickwhyte.com>
2015-09-19 18:29:32 +1000

    plzfix

--- Application/models/Project.py
+++ Application/models/Project.py
@@ -4,5 +4,6 @@ class Project(db.Document):
 	name = db.StringField()
 	date_uploaded = db.CreatedField()
 	primary_image = db.AnythingField(required=False)
-	images = db.ListField()
-	devs = db.SetField(db.DocumentField())
+	# images = db.ListField()
+	# devs = db.SetField(db.DocumentField())
+    #TODO JB: FIX THAT ^
