47cd287802a0cac350cf3dcc9d8230955da32bc5
Nicholas Whyte <nick@nickwhyte.com>
2015-09-19 10:41:50 +1000

    Add venv and vagrant working dir to gitignore

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+apt-get update
+apt-get install -y python2.7 python3.4 python3-dev python-dev
+apt-get install -y mongodb redis-server
+
+# Tell the bootstrap where the project is mounted
+PROJECT_ROOT="/deploy/com.csesoc.showcse/"
+PYTHON_BINARY="python3.4"
+
+# Install VirtualEnv
+easy_install pip
+easy_install3 pip
+pip install virtualenv
+
+# Make a VirtualEnv for the project.
+cd $PROJECT_ROOT;
+
+if [ -e ".venv" ]; then
+    rm -rf ".venv"; #Delete venv if it exists;
+fi
+
+virtualenv -p $PYTHON_BINARY ".venv"
+source ".venv/bin/activate"
+    # Inside Venv Context
+    # Install project requirements
+    pip install -r requirements.txt
+deactivate
