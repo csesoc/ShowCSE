1e5d389f83555c9b1cefb4af97b3a1964b8044ef
Nicholas Whyte <nick@nickwhyte.com>
2015-09-20 16:42:27 +1000

    Filter

--- Application/modules/frontend/controllers/Home.py
+++ Application/modules/frontend/controllers/Home.py
@@ -12,7 +12,7 @@ class Home(FlaskView):
 
     @classy_menu_item('frontend.home', 'Home', order=0)
     def index(self):
-        projects = Project.query.order_by(Project.name.asc())
+        projects = Project.query.filter(Project.name != None).order_by(Project.name.asc())
         showcase = Project.query.join(ProjectImage).order_by(Project.id.desc()).limit(3)
         top_projects = Project.query.order_by(Project.num_stars.desc()).limit(10)
 
