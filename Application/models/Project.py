e18f756929a7d1aa2f085de8a574795fbb102727
jake-waratahs <jakebloom744@gmail.com>
2015-09-20 11:22:59 +1000

    Project model now uses SLUG id

--- Application/models/Project.py
+++ Application/models/Project.py
@@ -1,16 +1,16 @@
 from Application import db
 
 class Project(db.Document):
-    id = db.IntField(required=True)
-    name = db.StringField(required=True)
-    date_uploaded = db.CreatedField()
-    primary_image = db.AnythingField(required=False)
-    description = db.StringField(required=True, default=None)
-    download_link = db.StringField(required=False, default=None)
-    website_link = db.StringField(required=False, default=None)
-    demo_link = db.StringField(required=False, default=None)
-    tags = db.SetField(db.StringField(), required=False, default=None)
-    images = db.ListField(db.AnythingField(required=False), required=False, default=None)
-    devs = db.SetField(db.DocumentField("User"), default=None)
-    stars = db.SetField(db.DocumentField("User"), default=None)
+   id = db.StringField(required=True)
+   name = db.StringField(required=True)
+   date_uploaded = db.CreatedField()
+   primary_image = db.AnythingField(required=False)
+   description = db.StringField(required=True, default=None)
+   download_link = db.StringField(required=False, default=None)
+   website_link = db.StringField(required=False, default=None)
+   demo_link = db.StringField(required=False, default=None)
+   tags = db.SetField(db.StringField(), required=False, default=None)
+   images = db.ListField(db.AnythingField(required=False), required=False, default=None)
+   devs = db.SetField(db.DocumentField("User"), default=None)
+   stars = db.SetField(db.DocumentField("User"), default=None)
 
