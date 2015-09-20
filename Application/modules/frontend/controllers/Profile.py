5ec64454acc1891359a918c779838b2e4d1da35c
jake-waratahs <jakebloom744@gmail.com>
2015-09-20 11:46:04 +1000

    Added user edit form and controller

--- Application/modules/frontend/controllers/Profile.py
+++ Application/modules/frontend/controllers/Profile.py
@@ -4,6 +4,7 @@ from flask_menu.classy import classy_menu_item
 from flask_login import current_user, login_required
 
 from Application.models import User
+from .forms import UserEditForm
 
 def show_menu():
     return current_user.is_authenticated
@@ -28,9 +29,21 @@ class Profile(FlaskView):
         return render_template('.profile/index.html', user=user)
 
     @login_required
-    @route('/edit/')
+    @route('/edit/', methods=['GET', 'POST'])
     def edit(self):
         form = UserEditForm()
 
         if form.submit.data and form.validate_on_submit:
             #update the user's details
+            current_user.website = form.website.data
+            current_user.github = form.github.data
+            current_user.email = form.email.data
+            current_user.about = form.about.data
+
+            current_user.save()
+
+            flash('Sucessfully updated your details!', 'success')
+            return redirect(url_for('.Profile:me'))
+
+        return render_template("edit_user.html", is_form=True,
+            form=form, user=current_user)
