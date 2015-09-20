98d59315db7c82d5667a497415ea0cca82bf5ef8
jake-waratahs <jakebloom744@gmail.com>
2015-09-20 15:13:01 +1000

    users

--- Application/models/User.py
+++ Application/models/User.py
@@ -3,6 +3,11 @@ from flask_login import UserMixin
 
 from .Project import Project
 
+users_followers = db.Table('users_followers',
+    db.Column('follower_id', db.String(20), db.ForeignKey('user.zid')),
+    db.Column('followee_id', db.String(20), db.ForeignKey('user.zid'))
+)
+
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     dn = db.Column(db.String(255))
@@ -14,14 +19,16 @@ class User(db.Model, UserMixin):
     program = db.Column(db.String(255))
     admin = db.Column(db.Boolean())
     about = db.Column(db.Text())
-    # followers = db.SetField(db.DocumentField("User"), default=None)
-    # following = db.SetField(db.DocumentField("User"), default=None)
+    following = db.relationship('User', secondary=users_followers,
+        primaryjoin=zid==users_followers.c.follower_id,
+        secondaryjoin=zid==users_followers.c.followee_id,
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    stars = db.relationship('Project', secondary='project_users_stars', 
+        lazy='dynamic')
+    projects = db.relationship('Project', secondary='project_users_devs', lazy='dynamic')
+
     def get_id(self):
         return self.zid
 
     def __hash__(self):
         return hash(self.zid)
-
-    stars = db.relationship('Project', secondary='project_users_stars', 
-        lazy='dynamic')
-    projects = db.relationship('Project', secondary='project_users_devs', lazy='dynamic')
