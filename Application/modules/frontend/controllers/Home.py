0efeb256b7c5203efb8b0d3e0cc5abfdac9df1c9
Nicholas Whyte <nick@nickwhyte.com>
2015-12-20 19:19:54 +1100

    Add directory endpoint, remove A-Z listing from homepage

--- Application/modules/frontend/controllers/Home.py
+++ Application/modules/frontend/controllers/Home.py
@@ -1,5 +1,5 @@
 from flask.ext.classy import FlaskView, route
-from flask import render_template
+from flask import render_template, request
 from flask_menu.classy import classy_menu_item
 from Application.models.Project import Project, ProjectImage, project_users_stars
 from Application.models.User import User, users_followers
@@ -12,25 +12,19 @@ class Home(FlaskView):
 
     @classy_menu_item('frontend.home', 'Home', order=0)
     def index(self):
-        projects = Project.query.filter(Project.name != None).order_by(
-            Project.name.asc())
         showcase = Project.query.filter(Project.num_images > 0).order_by(
             Project.id.desc()).limit(3)
         top_projects = Project.query.order_by(Project.num_stars.desc()).limit(10)
 
-        # following_subquery = db.session.query(users_followers).filter_by(
-            # follower_id=current_user.zid
-        # ).all()
-
-        # print(following_subquery)
-
-
-        # following_projects = Project.query.filter(
-        #     Project.
-        # ).order_by(Project.id.desc()).limit(10)
-
         return render_template('.home/index.html', 
-            projects=projects,
             showcase=showcase,
             top_projects=top_projects
         )
+
+    @classy_menu_item('frontend.directory', 'Directory', order=0)
+    def directory(self):
+        projects = Project.query.filter(Project.name != None).order_by(
+            Project.name.asc())
+
+        return render_template('.home/directory.html',
+            projects=projects)
