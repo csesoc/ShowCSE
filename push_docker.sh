c73646b6a8b01439d1a521b5782b62f3688432e8
Nicholas Whyte <nick@nickwhyte.com>
2016-07-27 17:52:49 +1000

    Fix wrong command

--- push_docker.sh
+++ push_docker.sh
@@ -15,6 +15,6 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 docker push csesoc/showcse:$REPO_TAG
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    docker tag docker tag csesoc/showcse:$REPO_TAG csesoc/showcse:latest;
+    docker tag csesoc/showcse:$REPO_TAG csesoc/showcse:latest;
     docker push csesoc/showcse:latest;
 fi
